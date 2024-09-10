a79c2af36c8cc494a311fafde07375cd9926ebec
kauefelippet <kaue.andrea.elias@gmail.com>
2024-09-09 22:34:07 -0300

    first commit & Hello, World!

---
+++ src/main/kotlin/com/walkwoof/walkwoof_api/HelloController.kt
@@ -0,0 +1,13 @@
+package com.walkwoof.walkwoof_api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello, World!"
+    }
+}
---
+++ src/main/kotlin/com/walkwoof/walkwoof_api/WalkwoofApiApplication.kt
@@ -0,0 +1,12 @@
+package com.walkwoof.walkwoof_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalkwoofApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WalkwoofApiApplication>(*args)
+
+}
